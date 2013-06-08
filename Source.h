e6bbc4c464577bdfeb4209bc8bc76df8feb4e470
Jing Liu <jing.leon@gmail.com>
2013-06-08 18:03:28 +0900

    added function to get time axis information

--- Source.h
+++ Source.h
@@ -3,6 +3,8 @@
 
 #include <TNamed.h>
 
+class TArrayD;
+
 class Source : public TNamed
 {
    protected:
@@ -15,10 +17,12 @@ class Source : public TNamed
       virtual Double_t N2(UShort_t type, Double_t time, Double_t energy)
       { return 0; }
       virtual Double_t Ne(UShort_t type, Double_t energy) { return 0; }
-      virtual Double_t Nt(UShort_t type, Double_t time) { return 0; }
 
       void SetDistance(Double_t distance) { fDistance=distance; }
       Double_t Distance() { return fDistance; }
+
+      virtual Int_t NbinsT() { return 0; }
+      virtual const TArrayD* BinEdgesT() { return 0; }
 
       ClassDef(Source,1);
 };
