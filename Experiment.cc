5c12efc579008947c106065492e22a3e1962d939
Jing Liu <jing.leon@gmail.com>
2013-06-08 18:02:37 +0900

    added N2

--- Experiment.cc
+++ Experiment.cc
@@ -7,6 +7,7 @@
 using namespace MAD;
 
 #include <TF1.h>
+#include <TH2D.h>
 #include <TAxis.h>
 #include <TMath.h>
 using namespace TMath;
@@ -23,7 +24,10 @@ Experiment::Experiment(Material *material, Source *source) :
 {
    for (UShort_t i=0; i<7; i++) {
       fXSxNe[i]=0;
+      fXSxN2[i]=0;
       fNevt[i]=0;
+      fHN2[i]=0;
+      fHNt[i]=0;
    }
 }
 
@@ -43,6 +47,28 @@ Double_t Experiment::XSxNe(Double_t *x, Double_t *parameter)
       return dXS*(fSource->Ne(1,Ev) + fSource->Ne(2,Ev) + 4*fSource->Ne(3,Ev));
 
    return dXS * fSource->Ne(type,Ev);
+}
+
+//______________________________________________________________________________
+//
+
+Double_t Experiment::XSxN2(Double_t *x, Double_t *parameter)
+{
+   Double_t Ev = x[0]*MeV; // neutrino energy
+   Double_t Er = parameter[0]*keV; // nuclear recoil energy
+   UShort_t type = static_cast<UShort_t>(parameter[1]); // type of neutrino
+   Double_t time = parameter[2]*second;
+
+   Element *element = (Element*) fMaterial->GetElement();
+   Double_t dXS = element->CNNSdXS(Er, Ev);
+
+   //Printf("t: %f s, Er: %f keV, Ev: %f MeV",time/second,Er/keV,Ev/MeV);
+   if (type<1 || type>6)
+      return dXS*(fSource->N2(1,time,Ev)/second/MeV
+            + fSource->N2(2,time,Ev)/second/MeV 
+            + 4*fSource->N2(3,time,Ev))/second/MeV;
+
+   return dXS * fSource->N2(type,time,Ev)/second/MeV;
 }
 
 //______________________________________________________________________________
@@ -82,38 +108,68 @@ Double_t Experiment::FuncN(Double_t *x, Double_t *parameter)
 //______________________________________________________________________________
 //
 
-Double_t Experiment::Nevt(UShort_t type,
-      Double_t nuclearRecoilEnergy, Double_t maxNeutrinoEnergy)
+Double_t Experiment::N2(UShort_t type, Double_t time, Double_t Enr)
 {
-   Double_t x[1] = {nuclearRecoilEnergy/keV};
-   Double_t p[2] = {maxNeutrinoEnergy/MeV, type};
+   if (!fMaterial) {
+      Warning("Nevt", "Please set targe material!");
+      return 0;
+   }
+   if (fMaterial->Nelements()!=1) {
+      Warning("Nevt", "Can only handle material with one element!");
+      return 0;
+   }
+   if (!fSource) {
+      Warning("Nevt", "Please set source of neutrino!");
+      return 0;
+   }
+
+   Element *element = (Element*) fMaterial->GetElement();
+   Double_t atomicMass  = element->A();
+   Double_t nNuclei = fMass/atomicMass*Avogadro;
+   Double_t distance = fSource->Distance()/hbarc;
+   Double_t area = 4*pi*distance*distance;
+
+   Double_t minEv = (Enr + Sqrt(2*element->M()*Enr))/2;
+   Double_t maxEv = 82.5*MeV; // max neutrino energy
+
+   TF1 *f = FXSxN2(type,time,Enr,minEv,maxEv);
+   return nNuclei/area*1e50*f->Integral(minEv/MeV, maxEv/MeV)*keV*second;
+}
+
+//______________________________________________________________________________
+//
+
+Double_t Experiment::Nevt(UShort_t type,
+      Double_t nEr, Double_t maxEv)
+{
+   Double_t x[1] = {nEr/keV};
+   Double_t p[2] = {maxEv/MeV, type};
    return FuncN(x,p);
 }
 
 //______________________________________________________________________________
 //
 
-TF1* Experiment::FNevt(UShort_t type,
-      Double_t maxNuclearRecoilEnergy, Double_t maxNeutrinoEnergy)
+TF1* Experiment::FNevt(UShort_t type, Double_t maxEr, Double_t maxEv)
 {
    if (fNevt[type]) {
       Double_t min, max;
       fNevt[type]->GetRange(min,max);
-      if (max!=maxNuclearRecoilEnergy/keV) {
+      if (max!=maxEr/keV) {
          Info("FNevt","Reset range of recoil energy.");
-         fNevt[type]->SetRange(0,maxNuclearRecoilEnergy/keV);
+         fNevt[type]->SetRange(0,maxEr/keV);
       }
-      if (fNevt[type]->GetParameter(0)!=maxNeutrinoEnergy/MeV) {
+      if (fNevt[type]->GetParameter(0)!=maxEv/MeV) {
          Info("FNevt","Reset maximal neutrino energy.");
-         fNevt[type]->SetParameter(0,maxNeutrinoEnergy/MeV);
+         fNevt[type]->SetParameter(0,maxEv/MeV);
       }
       return fNevt[type];
    }
 
    fNevt[type] = new TF1(Form("fNevt_%s_%s_%f_%d",
             fMaterial->GetName(), fSource->GetName(), fMass, type),
-         this, &Experiment::FuncN, 0, maxNuclearRecoilEnergy/keV,2);
-   fNevt[type]->SetParameter(0,maxNeutrinoEnergy/MeV);
+         this, &Experiment::FuncN, 0, maxEr/keV,2);
+   fNevt[type]->SetParameter(0,maxEv/MeV);
    fNevt[type]->SetParameter(1,type);
 
    if (type==0) {
@@ -134,26 +190,26 @@ TF1* Experiment::FNevt(UShort_t type,
 //______________________________________________________________________________
 //
 
-TF1* Experiment::FXSxNe(UShort_t type, Double_t nuclearRecoilEnergy,
-      Double_t minNeutrinoEnergy, Double_t maxNeutrinoEnergy)
+TF1* Experiment::FXSxNe(UShort_t type, Double_t nEr,
+      Double_t minEv, Double_t maxEv)
 {
    if (fXSxNe[type]) {
-      fXSxNe[type]->SetRange(minNeutrinoEnergy/MeV,maxNeutrinoEnergy/MeV);
-      fXSxNe[type]->SetParameter(0,nuclearRecoilEnergy/keV);
+      fXSxNe[type]->SetRange(minEv/MeV,maxEv/MeV);
+      fXSxNe[type]->SetParameter(0,nEr/keV);
       return fXSxNe[type];
    }
 
    fXSxNe[type] = new TF1(Form("fXSxNe%s%s%f%d",
             fSource->GetName(), fMaterial->GetName(), fMass, type), this, 
-         &Experiment::XSxNe, minNeutrinoEnergy/MeV, maxNeutrinoEnergy/MeV,2);
-   fXSxNe[type]->SetParameter(0,nuclearRecoilEnergy/keV);
+         &Experiment::XSxNe, minEv/MeV, maxEv/MeV,2);
+   fXSxNe[type]->SetParameter(0,nEr/keV);
    fXSxNe[type]->SetParameter(1,type);
 
    if (type==0) {
       fXSxNe[type]->SetTitle(Form(
                "%s, target: %s, recoil energy: %.1f keV;neutrino energy [MeV];1/MeV^{4}", 
                fSource->GetName(), fMaterial->GetTitle(), 
-               nuclearRecoilEnergy/keV));
+               nEr/keV));
       fXSxNe[type]->SetLineColor(kGray+2);
       fXSxNe[type]->SetLineWidth(2);
    } else {
@@ -168,22 +224,55 @@ TF1* Experiment::FXSxNe(UShort_t type, Double_t nuclearRecoilEnergy,
 //______________________________________________________________________________
 //
 
-TH1D* Experiment::HNevt(UShort_t type,
-      Double_t maxNuclearRecoilEnergy, Double_t maxNeutrinoEnergy)
+TF1* Experiment::FXSxN2(UShort_t type, Double_t time, 
+      Double_t Enr, Double_t minEv, Double_t maxEv)
 {
-   TH1D *h = (TH1D*) 
-      FNevt(type,maxNuclearRecoilEnergy,maxNeutrinoEnergy)->GetHistogram();
+   if (fXSxN2[type]) {
+      fXSxN2[type]->SetRange(minEv/MeV,maxEv/MeV);
+      fXSxN2[type]->SetParameter(0,Enr/keV);
+      fXSxN2[type]->SetParameter(2,time/second);
+      return fXSxN2[type];
+   }
+
+   fXSxN2[type] = new TF1(Form("fXSxN2%s%s%f%d",
+            fSource->GetName(), fMaterial->GetName(), fMass, type), this, 
+         &Experiment::XSxN2, minEv/MeV, maxEv/MeV,3);
+   fXSxN2[type]->SetParameter(0,Enr/keV);
+   fXSxN2[type]->SetParameter(1,type);
+   fXSxN2[type]->SetParameter(2,time/second);
+
+   if (type==0) {
+      fXSxN2[type]->SetTitle(Form(
+               "%s, target: %s, recoil energy: %.1f keV, time: %.1f second;neutrino energy [MeV];1/MeV^{4}", 
+               fSource->GetName(), fMaterial->GetTitle(), Enr/keV, time/second));
+      fXSxN2[type]->SetLineColor(kGray+2);
+      fXSxN2[type]->SetLineWidth(2);
+   } else {
+      fXSxN2[type]->SetTitle(Form(
+               "neutrino %d from %s, target: %s, recoil energy : %.1f, time: %.1f second;neutrino energy [MeV];1/MeV^{4}", 
+               type, fSource->GetName(), fMaterial->GetTitle(),
+               Enr/keV, time/second));
+      fXSxN2[type]->SetLineColor(type);
+   }
+   return fXSxN2[type];
+}
+
+//______________________________________________________________________________
+//
+
+TH1D* Experiment::HNevt(UShort_t type, Double_t maxEr, Double_t maxEv)
+{
+   TH1D *h = (TH1D*) FNevt(type,maxEr,maxEv)->GetHistogram();
    return h;
 }
 
 //______________________________________________________________________________
 //
 
-TH1D* Experiment::HXSxNe(UShort_t type, Double_t nuclearRecoilEnergy,
-      Double_t minNeutrinoEnergy, Double_t maxNeutrinoEnergy)
+TH1D* Experiment::HXSxNe(UShort_t type, Double_t nEr,
+      Double_t minEv, Double_t maxEv)
 {
-   TH1D *h = (TH1D*) FXSxNe(type, nuclearRecoilEnergy, 
-         minNeutrinoEnergy, maxNeutrinoEnergy)->GetHistogram();
+   TH1D *h = (TH1D*) FXSxNe(type, nEr, minEv, maxEv)->GetHistogram();
    return h;
 }
 
@@ -201,6 +290,73 @@ void Experiment::Clear(Option_t *option)
          delete fXSxNe[i];
          fXSxNe[i]=NULL;
       }
+      if (fXSxN2[i]) {
+         delete fXSxN2[i];
+         fXSxN2[i]=NULL;
+      }
+      if (fHN2[i]) {
+         delete fHN2[i];
+         fHN2[i]=NULL;
+      }
+      if (fHNt[i]) {
+         delete fHNt[i];
+         fHNt[i]=NULL;
+      }
    }
+}
+
+//______________________________________________________________________________
+//
+
+TH2D* Experiment::HN2(UShort_t type)
+{
+   if (fHN2[type]) return fHN2[type];
+
+   Int_t nbinst=fSource->NbinsT();
+   const Double_t *tbins = fSource->BinEdgesT()->GetArray();
+   fHN2[type] = new TH2D(Form("hN2%d",type),"",nbinst,tbins,100,0,50);
+
+   for (Int_t ix=1; ix<=fHN2[type]->GetNbinsX(); ix++) {
+      for (Int_t iy=1; iy<=fHN2[type]->GetNbinsY(); iy++) {
+         Double_t time = fHN2[type]->GetXaxis()->GetBinCenter(ix);
+         Double_t enur = fHN2[type]->GetYaxis()->GetBinCenter(iy);
+         Double_t nevt = N2(type,time*second,enur*keV);
+         fHN2[type]->SetBinContent(ix, iy, nevt);
+      }
+   }
+   fHN2[type]->SetStats(0);
+   fHN2[type]->GetXaxis()->SetTitle("time [second]");
+   fHN2[type]->GetYaxis()->SetTitle("nuclear recoil energy [keV]");
+   fHN2[type]->SetTitle(Form("number of events / (%.0f kg)",fMass/kg));
+   
+   return fHN2[type];
+}
+
+//______________________________________________________________________________
+//
+
+TH1D* Experiment::HNt(UShort_t type)
+{
+   if (fHNt[type]) return fHNt[type];
+
+   Int_t nbinst=fSource->NbinsT();
+   const Double_t *tbins = fSource->BinEdgesT()->GetArray();
+   fHNt[type] = new TH1D(Form("hNt%d",type),"",nbinst,tbins);
+
+   for (Int_t ix=1; ix<=fHNt[type]->GetNbinsX(); ix++) {
+      Double_t nevt=0;
+      for (Int_t iy=1; iy<=HN2(type)->GetNbinsY(); iy++) {
+         Double_t dn = HN2(type)->GetBinContent(ix,iy);
+         Double_t er = HN2(type)->GetYaxis()->GetBinWidth(iy);
+         nevt+=dn*er;
+      }
+      fHNt[type]->SetBinContent(ix, nevt);
+   }
+   fHNt[type]->SetStats(0);
+   fHNt[type]->GetXaxis()->SetTitle("time [second]");
+   fHNt[type]->GetYaxis()->SetTitle(Form(
+            "rate of events [Hz/(%.0f kg)]",fMass/kg));
+   
+   return fHNt[type];
 }
 
