d65fa154a204e2a5a961128f494a3f0ae68cda53
Jing Liu <jing.leon@gmail.com>
2013-06-13 11:45:05 +0900

    removed some default valves

--- SupernovaExperiment.h
+++ SupernovaExperiment.h
@@ -54,14 +54,12 @@ class SupernovaExperiment : public TNamed
       void SetDistance(Double_t distance) { fDistance=distance; }
       Double_t Distance() { return fDistance; }
 
-      TF1* FXSxNe(UShort_t type=1, 
-            Double_t nEr=5*UNIC::keV);
+      TF1* FXSxNe(UShort_t type, Double_t nEr);
 
       TF1* FNevt(UShort_t type=1,
             Double_t maxEr=50*UNIC::keV);
 
-      TH1D* HXSxNe(UShort_t type=1,
-            Double_t nEr=5*UNIC::keV);
+      TH1D* HXSxNe(UShort_t type, Double_t nEr);
 
       TH1D* HNevt(UShort_t type=1,
             Double_t maxEr=50*UNIC::keV);
@@ -69,7 +67,7 @@ class SupernovaExperiment : public TNamed
       Double_t N2(UShort_t type, Double_t time, Double_t Enr);
       TH2D* HN2(UShort_t type); // Nevt(t, Enr)
       TH1D* HNt(UShort_t type); // Nevt(t)
-      TF1* FXSxN2(UShort_t type=1, Double_t time=0, Double_t Enr=5*UNIC::keV);
+      TF1* FXSxN2(UShort_t type, Double_t time, Double_t Enr);
 
       /**
        * Delete internal objects.
