279035948a2b00f75e31cc2acb20ac9bb7506d28
Jing Liu <jing.leon@gmail.com>
2013-06-16 17:16:15 +0900

    added more classes

--- LinkDef.h
+++ LinkDef.h
@@ -1,3 +1,6 @@
 #ifdef __MAKECINT__
+#pragma link C++ class Detector+;
+#pragma link C++ class ScintillationDetector+;
+#pragma link C++ class LXeDetector+;
 #pragma link C++ class SupernovaExperiment+;
 #endif
